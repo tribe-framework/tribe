ccd28ec1ccbc7851bd93b81b429d9ce996aca952
akshay / wildfire <akshay.wildfire@pm.me>
2020-11-27 19:35:16 +0530

    init to core

--- tribe.init.php
+++ tribe.init.php
@@ -1,9 +1,4 @@
 <?php
-//session and vars
-session_save_path('/tmp');
-session_start();
-$session_user = $_SESSION['user'] ?? NULL;
-
 //composer autoload
 require __DIR__ . '/vendor/autoload.php';
 
@@ -14,24 +9,6 @@ $dotenv->load();
 //php vars file
 include_once __DIR__ . '/config/vars.php';
 
-// browser debugging
-if (defined('ENV') && (ENV == 'dev')) {
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-} else {
-    ini_set('display_errors', 0);
-    ini_set('display_startup_errors', 0);
-    error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
-}
-
-// wildfire core init
-$sql = new Wildfire\Core\MySQL();
-$dash = new Wildfire\Core\Dash();
-
-$types=$dash->get_types(ABSOLUTE_PATH.'/config/types.json');
-$menus=json_decode(file_get_contents(ABSOLUTE_PATH.'/config/menus.json'), true);
-
-$theme = new Wildfire\Core\Theme();
-$admin = new Wildfire\Core\Admin();
+//wildfire core classes
+include_once __DIR__ . '/vendor/wildfire/core/init.php';
 ?>
