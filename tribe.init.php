6591c07a7b27253dcabec02047a77e2b699132a1
akshay / jigmet <akshay.wildfire@pm.me>
2020-12-15 23:09:34 +0530

    using the word "Init" instead of "Kernal"

--- tribe.init.php
+++ tribe.init.php
@@ -2,17 +2,14 @@
 //composer autoload
 require __DIR__ . '/vendor/autoload.php';
 
-define('ROOT_DIR', __DIR__);
-
 //dotenv for loading variables in tribe.var.env as $_ENV
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '.env');
 $dotenv->load();
 
-date_default_timezone_set($_ENV['DEFAULT_TIMEZONE']);
-
 //php vars file
-include_once __DIR__.'/config/config.php';
+include_once __DIR__ . '/config/config.php';
 
-// initialising Wildfire Core
-$kernel = new Wildfire\Core\Kernel();
-$kernel->init();
+//wildfire core classes
+$init = new Wildfire\Core\Init();
+$init->init();
+?>
