04ae2a2bd0dc30d97fff1ef48419e03e35a7aaef
akshay / tw <akshay.wildfire@pm.me>
2021-05-15 09:37:39 +0530

    modified scss init because it is different in every project

--- theme/assets/scss/init.php
+++ theme/assets/scss/init.php
@@ -3,15 +3,8 @@ $sass = new Sass();
 $sass->setStyle(Sass::STYLE_COMPRESSED);
 $sass->setEmbed(true);
 
-$sass_file = $_GET['file'] ?? null;
-
-/**
- * if requested file is "bootstrap" then load bootstrap.scss
- * otherwise load .scss file from "scss/"
- */
-if ($sass_file == 'bootstrap') {
-	$css = $sass->compileFile(__DIR__ . '/bootstrap/bootstrap.scss');
-}
+//to use SCSS uncomment the line below and modify path to scss file if required
+//$css = $sass->compileFile(__DIR__ . '/bootstrap/bootstrap.scss');
 
 if ($css) {
 	header("Content-Type: text/css; charset=utf-8");
