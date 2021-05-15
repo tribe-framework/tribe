e23686ddf11c91dd65b05f00f8cf3c51c76d2eeb
akshay / tw <akshay.wildfire@pm.me>
2021-05-16 01:18:04 +0530

    scss init, comments and modifications

--- theme/assets/scss/init.php
+++ theme/assets/scss/init.php
@@ -1,12 +1,20 @@
 <?php
-$sass = new Sass();
-$sass->setStyle(Sass::STYLE_COMPRESSED);
-$sass->setEmbed(true);
+$scss_file_path = '';
+/*
+Read Bootstrap SCSS guide at https://getbootstrap.com/docs/5.0/customize/sass/
 
-//to use SCSS uncomment the line below and modify path to scss file if required
-//$css = $sass->compileFile(__DIR__ . '/bootstrap/bootstrap.scss');
+To modify Bootstrap's default SCSS, read about custom.scss in https://getbootstrap.com/docs/5.0/customize/sass/#importing
 
-if ($css) {
+To use SCSS modify path to scss file if required, uncomment the line below
+ */
+
+//$scss_file_path = __DIR__ . '/../../../node_modules/bootstrap/scss/bootstrap.scss';
+
+if ($scss_file_path) {
+	$sass = new Sass();
+	$sass->setStyle(Sass::STYLE_COMPRESSED);
+	$sass->setEmbed(true);
+	$css = $sass->compileFile($scss_file_path);
 	header("Content-Type: text/css; charset=utf-8");
 	echo $css;
 }
