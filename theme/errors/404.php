d6cd62d82a1337dd8c33ab78f223cc6ab84f96a7
akshay / tw <akshay.wildfire@pm.me>
2021-05-18 20:40:42 +0530

    404 relocation

--- theme/errors/404.php
+++
@@ -1,7 +0,0 @@
-<?php include_once __DIR__ . '../pages/_header.php'?>
-
-<div class="mx-auto mt-5">
-  <h1 class="display-4">404 / Nothing found.</h1>
-</div>
-
-<?php include_once __DIR__ . '../pages/_footer.php'?>
