2590382b2d5b062e110526b39a8cff07d6b70b5d
akshay / tw <akshay.wildfire@pm.me>
2021-05-28 15:46:43 +0530

    currentUser instead of session_user

--- theme/includes/_init.php
+++ theme/includes/_init.php
@@ -4,10 +4,11 @@ namespace Wildfire;
 $dash = new Core\Dash();
 $admin = new Core\Admin();
 $sql = new Core\MySQL();
+$auth = new Auth\Auth();
 
 $types = $dash->getTypes();
 $menus = $dash->getMenus();
-$session_user = $dash->getSessionUser();
+$currentUser = $auth->getCurrentUser();
 
 include_once __DIR__ . '/functions.php';
 $functions = new Theme\Functions();
