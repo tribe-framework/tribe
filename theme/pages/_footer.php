0cff14779f5e3ef6ecb9c4128f1424725965550c
Manav Yadav <manav.y1998@gmail.com>
2021-07-07 15:05:18 +0530

    <header> and <footer> tags added.

--- theme/pages/_footer.php
+++ theme/pages/_footer.php
@@ -1,5 +1,6 @@
-
-	<script src="/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-	<script src="/theme/assets/js/custom.js"></script>
+	<footer>
+		<script src="/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+		<script src="/theme/assets/js/custom.js"></script>
+	</footer>
 </body>
 </html>
