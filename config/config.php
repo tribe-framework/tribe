aecc66739ff478c3b91b0b2a14c249f476c5cb38
akshay / wildfire <akshay.wildfire@pm.me>
2021-07-20 13:20:56 +0530

    DB_PORT

--- config/config.php
+++ config/config.php
@@ -46,6 +46,7 @@ define('DB_NAME', $_ENV['DB_NAME']);
 define('DB_USER', $_ENV['DB_USER']);
 define('DB_PASS', $_ENV['DB_PASS']);
 define('DB_HOST', $_ENV['DB_HOST']);
+define('DB_PORT', $_ENV['DB_PORT']);
 define('BARE_URL', $_ENV['WEB_BARE_URL']);
 define('CONTACT_EMAIL', $_ENV['CONTACT_EMAIL']);
 define('WEBSITE_NAME', $_ENV['WEBSITE_NAME']);
