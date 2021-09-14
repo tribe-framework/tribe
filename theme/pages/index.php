64228ec4487cf504cbac868ea7e086c0b939b476
akshay / tw <akshay.wildfire@pm.me>
2021-09-14 12:38:15 +0530

    Creation of first user

--- theme/pages/index.php
+++ theme/pages/index.php
@@ -1,5 +1,19 @@
 <?php include_once __DIR__ . '/_header.php'?>
 
+<?php
+// YOU CAN REMOVE AFTER FIRST VISIT
+$q = $sql->executeSQL("SELECT `id` FROM `data` WHERE `type`='user'");
+if (!$q[0]['id']) {
+    $usr = array();
+    $usr['type'] = 'user';
+    $usr['role_slug'] = 'admin';
+    $usr['email'] = $_ENV['CONTACT_EMAIL'];
+    $usr['password'] = md5($_ENV['DB_PASS']);
+    $usr['user_id'] = $dash->get_unique_user_id();
+    $dash->push_content($usr);
+}
+?>
+
 <main class="container mt-5">
     <div class="starter-template text-center py-5 px-3">
         <h1>
@@ -15,6 +29,7 @@
         <p class="mt-4 text-muted">
             A Conversation with <a href="https://creativecloud.adobe.com/cc/discover/article/a-conversation-with-milton-glaser?locale=en">Milton Glaser</a>.
         </p>
+
     </div>
 </main>
 
