c3f4a54508c5b0ed669c3862a0b3a789e9c0b0ab
Apurv Jyotirmay <mail@jyotirmay.dev>
2021-12-22 14:54:35 +0530

    moved content from tribe.init to index

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
-include_once 'tribe.init.php';
+//composer autoload
+require __DIR__ . '/vendor/autoload.php';
+
+//dotenv for loading variables in tribe.var.env as $_ENV
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '.env');
+$dotenv->load();
+
+//php vars file
+include_once __DIR__ . '/config/config.php';
+
+//wildfire core classes
+$tribe_init = new Wildfire\Core\Init();
