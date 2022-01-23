0da5fe30261a58030d990f6b8f793872251838d3
Apurv Jyotirmay <mail@jyotirmay.dev>
2022-01-23 23:32:41 +0530

    removed error handling

--- index.php
+++ index.php
@@ -2,18 +2,4 @@
 require __DIR__ . '/init.php';
 
 // initialize tribe framework
-try {
-    new \Wildfire\Core\Init();
-} catch (\Throwable $t) {
-    $last_error = error_get_last();
-
-    if ('dev' == strtolower(($_ENV['ENV']))) {
-        \Wildfire\Core\Console::debug($last_error ?? $t);
-    }
-} catch (\Exception $e) {
-    $last_error = error_get_last();
-
-    if ('dev' == strtolower(($_ENV['ENV']))) {
-        \Wildfire\Core\Console::debug($last_error ?? $e);
-    }
-}
+new \Wildfire\Core\Init();
