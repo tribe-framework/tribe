0f24952497067b97dc452d912a8d438b975cc48e
Apurv Jyotirmay <mail@jyotirmay.dev>
2022-02-04 19:29:50 +0530

    removed error handler

--- _init.php
+++ _init.php
@@ -8,22 +8,3 @@ $dotenv->load();
 
 //php vars file
 require_once __DIR__.'/config/config.php';
-
-function tribe_error_handler()
-{
-    $last_error = error_get_last();
-
-    if (!($last_error && $last_error['type'] == E_ERROR)) {
-        return;
-    }
-
-    header("HTTP/1.1 500 Internal Server Error");
-    if (\file_exists(THEME_PATH.'/pages/500.php')) {
-        require_once THEME_PATH.'/pages/500.php';
-    } else {
-        echo "Internal server error";
-    }
-}
-
-// critical error handler
-register_shutdown_function('tribe_error_handler');
