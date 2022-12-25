a0e205d3c93f8206299bd13130698f3b00d52ae3
akshay.wildfire / tw <akshay.wildfire@pm.me>
2022-12-25 16:20:42 +0530

    tribe / uploads

--- install/first-user.php
+++ install/first-user.php
@@ -1,16 +1,16 @@
 <?php
-include_once('_init.php');
+include_once('../_init.php');
 $sql = new \Tribe\MySQL();
 $core = new \Tribe\Core();
-
-$q = $sql->executeSQL("SELECT `id` FROM `data` WHERE `type`='user'");
-if (!$q[0]['id']) {
-    $usr = array();
-    $usr['type'] = 'user';
-    $usr['role_slug'] = 'admin';
-    $usr['email'] = $_ENV['CONTACT_EMAIL'];
-    $usr['password'] = $_ENV['DB_PASS'];
-    $usr['user_id'] = $core->getUniqueUserID();
-    $core->pushObject($usr);
-}
+//echo $core->getNextID();
+//echo $core->getUniqueUserID();
+//print_r($core->doShellCommand('ls'));
+/*
+$usr = array();
+$usr['type'] = 'user';
+$usr['role_slug'] = 'admin';
+$usr['email'] = $_ENV['CONTACT_EMAIL'];
+$usr['password'] = $_ENV['DB_PASS'];
+$usr['user_id'] = 'ABCDEF';
+print_r($core->getObject($core->pushObject($usr)));
 ?>
