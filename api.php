074446a1fecc25df5ea006e5b79141459b4ca60d
akshay.wildfire / tw <akshay.wildfire@pm.me>
2022-12-26 17:53:59 +0530

    ember data integration

--- api.php
+++ api.php
@@ -1,2 +1,20 @@
 <?php
 require __DIR__ . '/_init.php';
+use alsvanzelf\jsonapi\ResourceDocument;
+$config = new \Tribe\Config;
+$core = new \Tribe\Core;
+
+$url_parts = array_filter(explode('/', $_SERVER['REQUEST_URI']));
+$type = $url_parts[2] ?? false;
+$id = $url_parts[3] ?? false;
+$object = $config->getTypes();
+
+foreach ($object as $key => $value) {
+	echo $core->getTypeObjectsCount($key);
+}
+
+$document = new ResourceDocument($type=$type, 0);
+$document->add('modules', $object);
+$document->addExtensionMember('meta', 'bar', 'baz');
+
+$document->sendResponse();
