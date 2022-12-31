87167da5a90f42590fed6da9940df43393f071fa
akshay.wildfire / tw <akshay.wildfire@pm.me>
2023-01-01 02:54:35 +0530

    tuning for ember data

--- api.php
+++ api.php
@@ -1,6 +1,9 @@
 <?php
 require __DIR__ . '/_init.php';
+
+use alsvanzelf\jsonapi\CollectionDocument;
 use alsvanzelf\jsonapi\ResourceDocument;
+
 $config = new \Tribe\Config;
 $core = new \Tribe\Core;
 
@@ -20,4 +23,23 @@ if ($type == 'webapp') {
 	$document = new ResourceDocument($type=$type, 0);
 	$document->add('modules', $object);
 	$document->sendResponse();
+}
+
+else {
+	if (($type ?? false) && !($id ?? false)) {
+		if ($ids = $core->getIDs(array('type'=>$type), '=', 'AND', 'id', 'DESC', 10)) {
+			$objects = $core->getObjects($ids);
+			$i = 0;
+			foreach ($objects as $object) {
+				$documents[$i] = new ResourceDocument($type=$type, $object['id']);
+				$documents[$i]->add('modules', $object);
+				$i++;
+			}
+			$document = CollectionDocument::fromResources(...$documents);
+			$document->sendResponse();
+		} else {
+			$document = new ResourceDocument();
+			$document->sendResponse();
+		}
+	}
 }
