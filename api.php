cf2966946034e67794fbd766a3aaeb36e9680ca9
akshay.wildfire / tw <akshay.wildfire@pm.me>
2023-01-03 19:54:54 +0530

    search sql query api

--- api.php
+++ api.php
@@ -7,11 +7,14 @@ use alsvanzelf\jsonapi\ResourceDocument;
 $config = new \Tribe\Config;
 $core = new \Tribe\Core;
 $api = new \Tribe\API;
+$sql = new \Tribe\MySQL;
 $auth = new \Tribe\Auth;
 
-$url_parts = array_filter(explode('/', $_SERVER['REQUEST_URI']));
-$type = $url_parts[2] ?? false;
-$id = (int) $url_parts[3] ?? false;
+$url_parts = explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+$type = (string) ($url_parts[2] ?? '');
+$id = (int) ($url_parts[3] ?? 0);
+
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
 	if ($id) {
@@ -32,7 +35,7 @@ else if ($_SERVER['REQUEST_METHOD'] === 'PATCH') {
 	
 	$object = $core->getObject($core->pushObject($object));
 
-    $document = new ResourceDocument($type=$type, $object['id']);
+    $document = new ResourceDocument($type, $object['id']);
     $document->add('modules', $object);
 	$document->sendResponse();
 }
@@ -47,7 +50,7 @@ else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 	$object = $core->getObject($core->pushObject($object));
 
-    $document = new ResourceDocument($type=$type, $object['id']);
+    $document = new ResourceDocument($type, $object['id']);
     $document->add('modules', $object);
 	$document->sendResponse();
 }
@@ -63,7 +66,7 @@ else {
 			}
 		}
 
-		$document = new ResourceDocument($type=$type, 0);
+		$document = new ResourceDocument($type, 0);
 		$document->add('modules', $object);
 		$document->sendResponse();
 	}
@@ -71,10 +74,12 @@ else {
 	else if (($type ?? false) && !($id ?? false)) {
 		$show_public_objects_only = ($_GET['show_public_objects_only'] ?? false);
 
-		
 		//FILTERING
-		if ($_GET['page'] !== null) {
-			$limit = "{$_GET['page']['offset']}, {$_GET['page']['limit']}";
+		if (($_GET['page'] ?? false) !== null) {
+			if (!($_GET['page']['limit'] ?? false))
+				$limit = $_GET['page']['limit'] = 25;
+			if ($_GET['page']['offset'] ?? false)
+				$limit = "{$_GET['page']['offset']}, {$_GET['page']['limit']}";
 		}
 		else {
 			$limit = 25;
@@ -84,39 +89,48 @@ else {
 
 
 
-		if ($_GET['modules'] !== null) {
+		if ($_GET['modules'] ?? false) {
 
 			foreach ($_GET['modules'] as $key=>$value) {
 				$search_query[$key][] = $value;
 			}
-			
+				
+			$query_lines = array();
 			$search_keys = array_keys($search_query);
 			
 			foreach ($search_keys as $key) {
 				$search_query[$key] = array_unique($search_query[$key]);
-				$query_lines[] = " (`content`->>'$.".$key."' LIKE '".implode("' OR `content`->>'$.".$key."' LIKE '", $search_query[$key])."') ";
+				$query_lines[] = " ( LOWER(`content`->>'$.".$key."') LIKE '%".implode("%' OR LOWER(`content`->>'$.".$key."') LIKE '%", array_map('strtolower', array_values($search_query[$key])))."%' ) ";
 			}
 
-			$query = "SELECT `id` FROM `data` WHERE `type`='".$type."' AND ".implode(" AND ", $query_lines." LIMIT ".$limit."");
+			$query = "SELECT `id` FROM `data` WHERE `type`='".$type."' AND ".implode(" AND ", $query_lines)." LIMIT ".$limit;
 
-			$ids = $sql->executeSQL($query);
+			$ids = $sql->executeSQL($query);;
 
-			$objects = $core->getObjects($ids);
-			$i = 0;
-			foreach ($objects as $object) {
-				$documents[$i] = new ResourceDocument($type=$type, $object['id']);
-				$documents[$i]->add('modules', $object);
-				$i++;
+			if ($ids) {
+				$objects = $core->getObjects($ids);
+				$i = 0;
+				foreach ($objects as $object) {
+					$documents[$i] = new ResourceDocument($type, $object['id']);
+					$documents[$i]->add('modules', $object);
+					$i++;
+				}
+				$document = CollectionDocument::fromResources(...$documents);
+				$document->sendResponse();
 			}
-			$document = CollectionDocument::fromResources(...$documents);
-			$document->sendResponse();
+			else {
+				$documents = [];
+				$document = CollectionDocument::fromResources(...$documents);
+				$document->sendResponse();
+			}
+
 		}
 
 		else if ($ids = $core->getIDs(array('type'=>$type), '=', 'AND', 'id', 'DESC', $limit, $show_public_objects_only)) {
 			$objects = $core->getObjects($ids);
 			$i = 0;
 			foreach ($objects as $object) {
-				$documents[$i] = new ResourceDocument($type=$type, $object['id']);
+				$documents[$i] = new ResourceDocument($type, $object['id']);
 				$documents[$i]->add('modules', $object);
 				$i++;
 			}
@@ -132,7 +146,7 @@ else {
 
 	else if (($type ?? false) && ($id ?? false)) {
 		if ($object = $core->getObject($id)) {
-			$document = new ResourceDocument($type=$type, $object['id']);
+			$document = new ResourceDocument($type, $object['id']);
 			$document->add('modules', $object);
 			$document->sendResponse();
 		} else {
