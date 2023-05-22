7990d4a7b2a496235e87eea5fab6e54372af8729
akshay.wildfire / tw <akshay.wildfire@pm.me>
2023-05-23 00:11:43 +0530

    tribe and ember nginx install instructions

--- install/ember.sh
+++ install/ember.sh
@@ -27,7 +27,9 @@ if [[ ${REPLY,,} =~ ^(y|yes|Y|YES|Yes)$ ]]; then
 	if [ -n "$tribedomain" ] && [ -n "$adminemail" ] && [ -n "$emberdomain" ] && [ -n "$emberdir" ]
 	then
 		#ember nginx conf + certbot
-		wget --no-cache --no-cookie https://raw.githubusercontent.com/tribe-framework/tribe/master/install/ember-nginx.conf /etc/nginx/sites-available/$emberdomain;
+		cd ~;
+		wget --no-cache --no-cookie https://raw.githubusercontent.com/tribe-framework/tribe/master/install/ember-nginx.conf;
+		mv ember-nginx.conf /etc/nginx/sites-available/$emberdomain;
 		sed -i "s/your_server_base_dir/$tribedomain/applications/$emberdir/g" /etc/nginx/sites-available/$emberdomain;
 		sed -i "s/your_server_domain/$emberdomain/g" /etc/nginx/sites-available/$emberdomain;
 		ln -s /etc/nginx/sites-available/$emberdomain /etc/nginx/sites-enabled/$emberdomain;
